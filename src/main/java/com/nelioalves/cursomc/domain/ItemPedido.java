cd4cf5521aa2684cf77185fc1fa9c37fb8b5b8e4
Rafael <dvr.rafa@gmail.com>
2022-04-05 18:41:35 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -0,0 +1,92 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity	
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L; 
+	
+	@EmbeddedId
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private double desconto;
+	private Integer quantidade;
+	private double preco;
+	
+	public ItemPedido() {}
+
+	public ItemPedido(Pedido pedido, Produto produto, double desconto, Integer quantidade, double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.desconto = desconto;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(desconto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Double.doubleToLongBits(desconto) == Double.doubleToLongBits(other.desconto);
+	}
+	
+	
+	
+	
+	
+}
